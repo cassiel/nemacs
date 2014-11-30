0b92a7ee7d4d638c6962068b90edc39cf3f0e372
nick (alibeykoy) <nick@cassiel.com>
2014-11-30 10:30:32 +0000

    Copy of .emacs-live.el.

---
+++ .emacs-live.el
@@ -0,0 +1,20 @@
+;;(live-use-packs '(live/foundation-pack live/colour-pack live/clojure-pack live/lang-pack live/power-pack live/git-pack))
+
+;;(live-add-packs '(live/git-pack))
+
+;;(defun gandalf ()
+;;  (interactive)
+;;  (color-theme-gandalf)
+;;  (set-face-background 'hl-line "#CCCCCC")
+;;  (set-face-attribute 'default nil :font "Anonymous Pro 12"))
+
+;;(defun cyberpunk ()
+;;  (interactive)
+;;  (color-theme-cyberpunk)
+;;  (set-face-background 'hl-line "#333333")
+;;  (set-face-attribute 'default nil :font "Anonymous Pro 12"))
+
+(live-add-packs '(~/GITHUB/cassiel/nemacs/packs/mobileorg
+                  ~/GITHUB/cassiel/nemacs/packs/org-mode-bindings
+                  ~/GITHUB/cassiel/nemacs/packs/miscellany
+                  ~/GITHUB/cassiel/nemacs/packs/buffer-move))
