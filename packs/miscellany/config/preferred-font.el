4ef19aaa4610d5dbe93871a44793461cd912fad4
Nick Rothwell (Kazlicesme) <nick@cassiel.com>
2014-12-10 21:13:05 +0000

    Font for Linux.

--- packs/miscellany/config/preferred-font.el
+++ packs/miscellany/config/preferred-font.el
@@ -5,4 +5,8 @@
            default-frame-alist)
 (cond
  ((and (window-system) (eq system-type 'darwin))
-  (add-to-list 'default-frame-alist '(font . "Courier 12"))))
+  (add-to-list 'default-frame-alist '(font . "Courier 12")))
+
+ ((and (window-system) (eq system-type 'gnu/linux))
+  (add-to-list 'default-frame-alist '(font . "7x14"))
+  ))
