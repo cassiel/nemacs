26cccd461a3b4efb761be9cbfe534d63b975f95c
Nick (Yedikule) <nick@cassiel.com>
2015-05-27 16:47:36 +0100

    Package update.

--- .emacs-live.el
+++ .emacs-live.el
@@ -14,9 +14,17 @@
 ;;  (set-face-background 'hl-line "#333333")
 ;;  (set-face-attribute 'default nil :font "Anonymous Pro 12"))
 
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(package-initialize)
+
 (live-add-packs '(~/GITHUB/cassiel/nemacs/packs/mobileorg
                   ~/GITHUB/cassiel/nemacs/packs/org-mode-bindings
                   ~/GITHUB/cassiel/nemacs/packs/miscellany
                   ~/GITHUB/cassiel/nemacs/packs/ibuffer
                   ~/GITHUB/cassiel/nemacs/packs/git-annex
+                  ~/GITHUB/cassiel/nemacs/packs/modes
                   ~/GITHUB/cassiel/nemacs/packs/buffer-move))
