4c872386988da9e35f68d571789194df87a197d6
Nick Rothwell <nick@cassiel.com>
2016-05-12 15:09:10 +0100

    Babel configuration.

--- packs/org-mode-bindings/config/bindings.el
+++ packs/org-mode-bindings/config/bindings.el
@@ -2,3 +2,7 @@
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cb" 'org-iswitchb)
+
+(define-key org-mode-map (kbd "<f12>") 'org-edit-special)
+(define-key org-src-mode-map (kbd "<f12>") 'org-edit-src-exit)
+(define-key org-mode-map (kbd "<M-f12>") 'org-babel-tangle)
--- packs/org-mode-bindings/config/config.el
+++ packs/org-mode-bindings/config/config.el
@@ -1,2 +1,26 @@
 (setq org-log-into-drawer t)
 (setq org-clock-into-drawer 1)
+
+;; Clojure support:
+(require 'ob-clojure)
+
+;; Evaluate always (slightly dangerous):
+(setq org-confirm-babel-evaluate nil)
+
+;; Work-round: transient buffers for fontification:
+(defun kill-org-src-buffers (&rest args)
+  "Kill temporary buffers created by
+  org-src-font-lock-fontify-block so they don't interfere with
+  magit-mode."
+  (dolist (b (buffer-list))
+    (let ((bufname (buffer-name b)))
+      (if (string-prefix-p " org-src-fontification:" bufname)
+          (kill-buffer b)))))
+
+(advice-add 'org-src-font-lock-fontify-block
+            :after #'kill-org-src-buffers)
+
+;; Table-of-contents (needs toc-org from MELPA):
+(if (require 'toc-org nil t)
+    (add-hook 'org-mode-hook 'toc-org-enable)
+  (warn "toc-org not found"))
