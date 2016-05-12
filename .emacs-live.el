e4cb4049c3ac74734aa7c5beb9fecec4d713afb8
Nick Rothwell <nick@cassiel.com>
2016-05-12 15:10:15 +0100

    org-mode-extras in .emacs-live.el.

--- .emacs-live.el
+++ .emacs-live.el
@@ -24,7 +24,7 @@
 (package-initialize)
 
 (live-add-packs '(~/GITHUB/cassiel/nemacs/packs/mobileorg
-                  ~/GITHUB/cassiel/nemacs/packs/org-mode-bindings
+                  ~/GITHUB/cassiel/nemacs/packs/org-mode-extras
                   ~/GITHUB/cassiel/nemacs/packs/miscellany
                   ~/GITHUB/cassiel/nemacs/packs/ibuffer
                   ~/GITHUB/cassiel/nemacs/packs/git-annex
