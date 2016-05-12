4b198b2c43d871b30cd972530043726199da25d5
Nick Rothwell <nick@cassiel.com>
2016-05-12 14:31:34 +0100

    Added MELPA (bleeding edge).

--- .emacs-live.el
+++ .emacs-live.el
@@ -18,6 +18,8 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
              '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 (package-initialize)
 
