5867a236f23083457d36e2707317c316690a4882
Nick Rothwell <nick@cassiel.com>
2016-07-19 12:09:45 +0100

    Default font (Mac).

--- packs/miscellany/config/preferred-font.el
+++ packs/miscellany/config/preferred-font.el
@@ -5,7 +5,8 @@
            default-frame-alist)
 (cond
  ((and (window-system) (eq system-type 'darwin))
-  (add-to-list 'default-frame-alist '(font . "Anonymous Pro 12")))
+  (add-to-list 'default-frame-alist '(font . "Input Mono 11")))
+ ;; was: "Anonymous Pro 12"
 
  ((and (window-system) (eq system-type 'gnu/linux))
   (add-to-list 'default-frame-alist '(font . "Monospace 8"))))
